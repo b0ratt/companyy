9d07673e02e88869ac70d7995320e9f49bb261e2
Borat <Borat@DESKTOP-S3RIRST>
2020-09-28 18:15:23 +0200

    unit tests + doxygen comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,12 +38,13 @@ namespace companyy
 
         }
 
+
+        /// Load data by setting the CollectionViewSource.Source property:
+        /// customersViewSource.Source = [generic data source]
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
 
             System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
-            /// Load data by setting the CollectionViewSource.Source property:
-            /// customersViewSource.Source = [generic data source]
 
             context.Customers.Load();
 
@@ -69,10 +70,15 @@ namespace companyy
         {
             custViewSource.View.MoveCurrentToFirst();
         }
+        /// <summary>
+        /// If existing window is visible, delete the customer and all their orders.
+        /// </summary>
+        /// <param name="sender">is a parameter called Sender that contains a reference to the control/object that raised the event.</param>
+        /// <param name="e">is a parameter called e that contains the event data, see the EventArgs MSDN page for more information.</param>
+        /// <returns>Deleting customer, refreshing window and displaying other customer with current orders</returns>
         private void DeleteCustomerCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
-            /// If existing window is visible, delete the customer and all their orders.  
-            /// In a real application, you should add warnings and allow the user to cancel the operation.  
+            
             var cur = custViewSource.View.CurrentItem as Customers;
 
             var cust = (from c in context.Customers
@@ -91,14 +97,19 @@ namespace companyy
             custViewSource.View.Refresh();
         }
 
+        /// <summary>
         /// Commit changes from the new customer form, the new order form,  
-        /// or edits made to the existing customer form.  
+        /// or edits made to the existing customer form.
+        /// </summary>
+        /// <param name="sender">is a parameter called Sender that contains a reference to the control/object that raised the event.</param>
+        /// <param name="e">is a parameter called e that contains the event data, see the EventArgs MSDN page for more information.</param>
+        /// <returns>Depending on active window (new customer or new order), data is being saved and window refreshed</returns>
         private void UpdateCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             if (newCustomerGrid.IsVisible)
             {
-                /// Create a new object because the old one  
-                /// is being tracked by EF now.  
+                // Create a new object because the old one  
+                // is being tracked by EF now.  
                 Customers newCustomer = new Customers
                 {
                     Address = add_addressTextBox.Text,
@@ -114,10 +125,10 @@ namespace companyy
                     Region = add_regionTextBox.Text
                 };
 
-                /// Perform very basic validation  
+                // Perform very basic validation  
                 if (newCustomer.CustomerID.Length == 5)
                 {
-                    /// Insert the new customer at correct position:  
+                    // Insert the new customer at correct position:  
                     int len = context.Customers.Local.Count();
                     int pos = len;
                     for (int i = 0; i < len; ++i)
@@ -142,9 +153,9 @@ namespace companyy
             }
             else if (newOrderGrid.IsVisible)
             {
-                /// Order ID is auto-generated so we don't set it here.  
-                /// For CustomerID, address, etc we use the values from current customer.  
-                /// User can modify these in the datagrid after the order is entered.  
+                // Order ID is auto-generated so we don't set it here.  
+                // For CustomerID, address, etc we use the values from current customer.  
+                // User can modify these in the datagrid after the order is entered.  
 
                 Customers currentCustomer = (Customers)custViewSource.View.CurrentItem;
 
@@ -174,10 +185,8 @@ namespace companyy
 
                 try
                 {
-                    /// Exercise for the reader if you are using Northwind:  
-                    /// Add the Northwind Shippers table to the model.
 
-                    /// Acceptable ShipperID values are 1, 2, or 3.  
+                    // Acceptable ShipperID values are 1, 2, or 3.  
                     if (add_ShipViaTextBox.Text == "1" || add_ShipViaTextBox.Text == "2"
                         || add_ShipViaTextBox.Text == "3")
                     {
@@ -204,26 +213,29 @@ namespace companyy
                     MessageBox.Show("Freight must be convertible to decimal.");
                     return;
                 }
-
-                /// Add the order into the EF model  
+                // Add the order into the EF model  
                 context.Orders.Add(newOrder);
                 ordViewSource.View.Refresh();
             }
 
-            /// Save the changes, either for a new customer, a new order  
-            /// or an edit to an existing customer or order.
+            // Save the changes, either for a new customer, a new order  
+            // or an edit to an existing customer or order.
             context.SaveChanges();
         }
-
+        /// <summary>
         /// Sets up the form so that user can enter data. Data is later  
         /// saved when user clicks Commit.  
+        /// </summary>
+        /// <param name="sender">is a parameter called Sender that contains a reference to the control/object that raised the event.</param>
+        /// <param name="e">is a parameter called e that contains the event data, see the EventArgs MSDN page for more information.</param>
+        /// <returns>Clear all the text boxes before adding a new customer. </returns>
         private void AddCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             existingCustomerGrid.Visibility = Visibility.Collapsed;
             newOrderGrid.Visibility = Visibility.Collapsed;
             newCustomerGrid.Visibility = Visibility.Visible;
 
-            /// Clear all the text boxes before adding a new customer.  
+             
             foreach (var child in newCustomerGrid.Children)
             {
                 var tb = child as TextBox;
@@ -249,7 +261,12 @@ namespace companyy
             newOrderGrid.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
         /// Cancels any input into the new customer form  
+        /// </summary>
+        /// <param name="sender">is a parameter called Sender that contains a reference to the control/object that raised the event.</param>
+        /// <param name="e">is a parameter called e that contains the event data, see the EventArgs MSDN page for more information.</param>
+        /// <returns>Clearing all inputs</returns>
         private void CancelCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             add_addressTextBox.Text = "";
@@ -271,29 +288,28 @@ namespace companyy
 
         private void Delete_Order(Orders order)
         {
-            /// Find the order in the EF model.  
+            // Find the order in the EF model.  
             var ord = (from o in context.Orders.Local
                        where o.OrderID == order.OrderID
                        select o).FirstOrDefault();
 
-            /// Delete all the order_details that have  
-            /// this Order as a foreign key  
+            // Delete all the order_details that have  
+            // this Order as a foreign key  
             foreach (var detail in ord.Order_Details.ToList())
             {
                 context.Order_Details.Remove(detail);
             }
 
-            /// Now it's safe to delete the order.  
+            // Now it's safe to delete the order.  
             context.Orders.Remove(ord);
             context.SaveChanges();
 
-            /// Update the data grid.  
+            // Update the data grid.  
             ordViewSource.View.Refresh();
         }
-
+        /// Get the Order in the row in which the Delete button was clicked
         private void DeleteOrderCommandHandler(object sender, ExecutedRoutedEventArgs e)
-        {
-            /// Get the Order in the row in which the Delete button was clicked.  
+        {  
             Orders obj = e.Parameter as Orders;
             Delete_Order(obj);
         }
